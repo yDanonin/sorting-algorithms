8a1fc926e033de1df3c6f8edfdbf8be848bccc12
ydanonin <fabianorocha102003@gmail.com>
2023-11-12 11:53:22 -0300

    Add testing ofthe algorithms

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
 #include "algs/sorting_algorithms.h"
 
-FILE openFile(char file_name[]) {
+FILE *openFile(char file_name[]) {
     char directory[128] = "/home/fabiano/CLionProjects/sorting-algorithms/data/";
     strncat(directory, file_name, sizeof(directory) - strlen(directory) - 1);
     printf("AQUI: %s", directory);
+
     if(access(directory, F_OK) == -1){
         perror("Erro ao acessar o arquivo");
     }
@@ -17,47 +20,49 @@ FILE openFile(char file_name[]) {
         perror("Erro ao abrir o arquivo");
     }
 
-    return *file;
+    return file;
+}
+
+void testSortingAlgorithm(const char* algorithmName, void (*sortFunction)(int[], int), int arr[], int size) {
+    clock_t start, end;
+    double cpu_time_used;
+
+    start = clock();
+    sortFunction(arr, size);
+    end = clock();
+
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    printf("%s took %f seconds to execute\n", algorithmName, cpu_time_used);
 }
 
 int main() {
     FILE *file;
-    char file_name[] = "ARQALEAT";
+    char file_name[] = "dtdecre10uni0.txt10uni1.txt";
     char line[100];
+    int dataArray[100];
+    int index = 0;
 
-    *file = openFile(file_name);
+    file = openFile(file_name);
 
     printf("\n");
     while (fgets(line, sizeof(line), file) != NULL) {
+        int value = atoi(line);
+        dataArray[index] = value;
+        index++;
         printf("%s", line);
     }
 
+    printf("Dados armazenados no array:\n");
+    for (int i = 0; i < index; i++) {
+        printf("%d\n", dataArray[i]);
+    }
     fclose(file);
 
-//    int arr[] = { 37, 23, 0, 17, 12, 72, 31, 46, 100, 88, 54 };
-//    int size = sizeof(arr) / sizeof(arr[0]);
-//
-//    printf("Vetor original:\n");
-//    for (int i = 0; i < size; i++) {
-//        printf("%d ", arr[i]);
-//    }
-//    printf("\n");
-//
-//    printf("------------------------------------- Starting Quicksort ------------------------------------- ");
-//
-//    printf("\n");
-//
-//    quick_sort(arr, size);
-//
-//    printf("------------------------------------- Quicksort Done ------------------------------------- ");
-//
-//    printf("\n");
-//    printf("\n");
-//
-//    printf("Vetor ordenado:\n");
-//    for (int i = 0; i < size; i++) {
-//        printf("%d ", arr[i]);
-//    }
-//    printf("\n");
+    testSortingAlgorithm("Quick Sort", quick_sort, dataArray, sizeof(dataArray) / sizeof(dataArray[0]));
+//    testSortingAlgorithm("Binary Insertion Sort", insertion_sort, dataArray, sizeof(dataArray) / sizeof(dataArray[0]));
+//    testSortingAlgorithm("Radix Sort LSD", radix_sort_lsd_start, dataArray, sizeof(dataArray) / sizeof(dataArray[0]));
+//    testSortingAlgorithm("Radix Sort MSD", radix_sort_msd_start, dataArray, sizeof(dataArray) / sizeof(dataArray[0]));
+
     return 0;
 }
